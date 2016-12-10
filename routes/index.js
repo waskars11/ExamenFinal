9b8ecbcc128bb60ab8fb26b5f735e4e9e6b20438
waskars11 <waskarsarias@gmail.com>
2016-12-10 10:14:05 -0400

    archivos

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Avances en Computacion' });
+  res.render('index', { title: 'Examen Final',subtitle:'Avances en Computacion' });
 });
 
 module.exports = router;
